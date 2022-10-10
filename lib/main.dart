127586671ca70ea667bad034d0600991a03cdef1
AvinandanBose <boseavinandan@gmail.com>
2022-10-10 21:16:53 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'fetch.dart';
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      // options: const FirebaseOptions
+      //   (apiKey: "AIzaSyBFuwoL_gF3fuERJmCSLBWAo1SoqP2Omys",
+      //     appId: "1:453604555763:web:35800392d33abbdae93086",
+      //     messagingSenderId:"453604555763",
+      //     projectId: "studentreg-c47ca",)
+      );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: fetch(),
+    );
+  }
+}
